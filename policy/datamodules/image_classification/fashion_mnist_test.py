f69d0d78e681894a3efcd422c56c74677bd17b6c
nMaax <massimilianocarli@proton.me>
2026-04-15 18:16:20 +0200

    Remove image classification datamodules and configs.

--- policy/datamodules/image_classification/fashion_mnist_test.py
+++
@@ -1,9 +0,0 @@
-from policy.conftest import setup_with_overrides
-from policy.datamodules.image_classification.fashion_mnist import FashionMNISTDataModule
-from policy.datamodules.image_classification.image_classification_test import (
-    ImageClassificationDataModuleTests,
-)
-
-
-@setup_with_overrides("datamodule=fashion_mnist")
-class TestFashionMNISTDataModule(ImageClassificationDataModuleTests[FashionMNISTDataModule]): ...
