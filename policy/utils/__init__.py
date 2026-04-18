0123037a70a6e171d7a96259615cf49bcb6845e6
nMaax <massimilianocarli@proton.me>
2026-04-18 16:42:17 +0200

    Added utils functions to init __all__ variable

--- policy/utils/__init__.py
+++ policy/utils/__init__.py
@@ -1,3 +1,3 @@
 from .utils import print_config, print_dict_tree, get_batch_size, flatten_tensor_dict
 
-__all__ = ["print_dict_tree", "print_config"]
+__all__ = ["print_dict_tree", "print_config", "get_batch_size", "flatten_tensor_dict"]
