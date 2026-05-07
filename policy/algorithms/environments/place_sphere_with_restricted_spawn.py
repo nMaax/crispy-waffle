8270cd13b4af6d84bbd7b4ef13f9149df3cf8efe
nMaax <massimilianocarli@proton.me>
2026-05-07 16:57:29 +0200

    Renamed Place Sphere with restricted spawn environment

--- policy/algorithms/environments/place_sphere_with_restricted_spawn.py
+++ policy/algorithms/environments/place_sphere_with_restricted_spawn.py
@@ -8,20 +8,20 @@ from mani_skill.utils.structs.pose import Pose
 
 @register_env("RestrctedSpawnPlaceSphere-v1", max_episode_steps=200)
 class PlaceSphereWithRestrictedSpawnEnv(PlaceSphereEnv):
+    # In-distribution ranges (<1*std) for sphere (Cube A) and bin (Cube B)
+    SPHERE_X_RANGE = (-0.08243, 0.07583)
+    SPHERE_Y_RANGE = (-0.12941, 0.13140)
+    BIN_X_RANGE = (-0.08909, 0.08211)
+    BIN_Y_RANGE = (-0.13197, 0.13146)
+
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
         super()._initialize_episode(env_idx, options)
 
-        # In-distribution ranges (<1*std) for sphere (Cube A) and bin (Cube B)
-        sphere_x_range = (-0.08243, 0.07583)
-        sphere_y_range = (-0.12941, 0.13140)
-        bin_x_range = (-0.08909, 0.08211)
-        bin_y_range = (-0.13197, 0.13146)
+        sphere_x = random.uniform(*self.SPHERE_X_RANGE)
+        sphere_y = random.uniform(*self.SPHERE_Y_RANGE)
 
-        sphere_x = random.uniform(*sphere_x_range)
-        sphere_y = random.uniform(*sphere_y_range)
-
-        bin_x = random.uniform(*bin_x_range)
-        bin_y = random.uniform(*bin_y_range)
+        bin_x = random.uniform(*self.BIN_X_RANGE)
+        bin_y = random.uniform(*self.BIN_Y_RANGE)
 
         with torch.device(self.device):
             current_obj_pose = self.obj.pose
@@ -36,5 +36,4 @@ class PlaceSphereWithRestrictedSpawnEnv(PlaceSphereEnv):
             new_bin_p = current_bin_pose.p.clone()
             new_bin_p[:, 0] = bin_x
             new_bin_p[:, 1] = bin_y
-
             self.bin.set_pose(Pose.create(new_bin_p, current_bin_pose.q))  # type: ignore
