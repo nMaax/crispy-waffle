1ca0916336298bd93ee6ffcbf817403afb8db323
nMaax <massimilianocarli@proton.me>
2026-05-15 19:22:34 +0200

    Fixed wrong imports and references

--- policy/adapters/__init__.py
+++ policy/adapters/__init__.py
@@ -1,8 +1,8 @@
 from .multi_task_neural_adapter import MultiTaskNeuralAdapter
 from .neural_adapter import NeuralAdapter
 from .no_op_adapter import NoOpAdapter
-from .place_sphere_to_stack_cube import PlaceSphereToStackCubeAdapter
-from .stack_cube_permuter import StackCubeSwappedToStackCubeAdapter
+from .place_sphere_to_stack_cube_adapter import PlaceSphereToStackCubeAdapter
+from .stack_cube_swapped_to_stack_cube_adapter import StackCubeSwappedToStackCubeAdapter
 
 __all__ = [
     "MultiTaskNeuralAdapter",
