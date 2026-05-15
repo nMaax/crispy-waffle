549b9641f9a5f087557cabad4ace16201d285062
nMaax <massimilianocarli@proton.me>
2026-05-15 19:02:34 +0200

    Decoupled MultiTaskStateTranslator Lightning Module from StateTranslator and saving mapping dictionary for tasks as hyperparameter

--- policy/adapters/multi_task_neural_adapter.py
+++ policy/adapters/multi_task_neural_adapter.py
@@ -2,7 +2,7 @@ from collections.abc import Sequence
 
 import torch
 
-from policy.algorithms.state_translator import StateTranslator
+from policy.algorithms.multi_task_state_translator import MultiTaskStateTranslator
 from policy.transforms import PnPCanonicalizer
 from policy.utils.typing_utils import AdapterProtocol
 
@@ -11,19 +11,19 @@ class MultiTaskNeuralAdapter(AdapterProtocol):
     def __init__(
         self,
         ckpt_path: str,
-        env_id: str,
-        env_idx: int,
-        task_to_idx: dict[str, int],
+        task_name: str,
         passthrough_mapping: Sequence[tuple[str, int]] | None = None,
     ):
-        self.model = StateTranslator.load_from_checkpoint(ckpt_path, strict=False)
+        self.model = MultiTaskStateTranslator.load_from_checkpoint(ckpt_path, strict=False)
         self.model.eval()
         self.model.freeze()
 
-        self.env_id = env_id
-        self.env_idx = env_idx
-        self.pnp_canonicalizer = PnPCanonicalizer(env_id)
-        self.task_mapping = task_to_idx
+        self.task_name = task_name
+
+        task_mapping = self.model.task_mapping
+        self.task_idx = task_mapping[task_name]
+
+        self.pnp_canonicalizer = PnPCanonicalizer(task_name)
 
         self.passthrough_mapping = passthrough_mapping
 
@@ -41,10 +41,10 @@ class MultiTaskNeuralAdapter(AdapterProtocol):
         # If obs is unbatched [39], task_idx must be a scalar []
         # If obs is batched [B, 39], task_idx must be [B]
         if obs.dim() == 1:
-            task_idx = torch.tensor(self.env_idx, dtype=torch.long, device=obs.device)
+            task_idx = torch.tensor(self.task_idx, dtype=torch.long, device=obs.device)
         else:
             task_idx = torch.full(
-                obs.shape[:-1], self.env_idx, dtype=torch.long, device=obs.device
+                obs.shape[:-1], self.task_idx, dtype=torch.long, device=obs.device
             )
 
         with torch.no_grad():
--- policy/algorithms/multi_task_state_translator.py
+++ policy/algorithms/multi_task_state_translator.py
@@ -1,9 +1,95 @@
+import functools
+
+import hydra_zen
+import lightning as L
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import LRScheduler
+from torch.optim.optimizer import Optimizer
 
-from .state_translator import StateTranslator
+from policy.transforms import TensorNormalizer
+from policy.utils.hydra_utils import parse_slice
+from policy.utils.typing_utils import HydraConfigFor
 
 
-class MultiTaskStateTranslator(StateTranslator):
+class MultiTaskStateTranslator(L.LightningModule):
+    def __init__(
+        self,
+        network: HydraConfigFor[nn.Module],
+        optimizer: HydraConfigFor[functools.partial[Optimizer]],
+        task_mapping: dict[str, int],
+        lr_scheduler: HydraConfigFor[functools.partial[LRScheduler]] | None = None,
+        loss_mask_slices: list[str | int] | None = None,
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        self.network_config = network
+        self.network: torch.nn.Module | None = None
+
+        self.optimizer_config = optimizer
+        self.optimizer: Optimizer | None = None
+
+        self.lr_scheduler_config = lr_scheduler
+        self.lr_scheduler: LRScheduler | None = None
+
+        self.x_normalizer = TensorNormalizer(network.input_dim)
+        self.y_normalizer = TensorNormalizer(network.output_dim)
+
+        self.loss_mask_slices = loss_mask_slices
+        self.task_mapping = task_mapping
+
+    def setup(self, stage: str) -> None:
+        if stage == "fit" and not self.x_normalizer.is_fit:
+            self._parse_loss_mask()
+            self._configure_normalizers()
+
+    def configure_model(self) -> None:
+        if self.network is not None:
+            return
+
+        self.network = hydra_zen.instantiate(self.network_config)
+
+    def configure_optimizers(self) -> Optimizer | dict:
+
+        # Optimizers and schedulers could actually be made in one shot, without partial,
+        # however I prefer to follow the template prescription, just for coherence
+
+        optimizer_partial = hydra_zen.instantiate(self.optimizer_config)
+        optimizer = optimizer_partial(self.parameters())
+
+        if self.lr_scheduler_config is not None:
+            lr_scheduler_partial = hydra_zen.instantiate(self.lr_scheduler_config)
+            lr_scheduler = lr_scheduler_partial(optimizer)
+
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "scheduler": lr_scheduler,
+                    "interval": "step",
+                    "frequency": 1,
+                },
+            }
+        else:
+            return optimizer
+
+    def training_step(self, batch, batch_idx):
+        loss = self._compute_loss(batch)
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._compute_loss(batch)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss = self._compute_loss(batch)
+        self.log("test/loss", loss, on_step=False, on_epoch=True, sync_dist=True)
+        return loss
+
     def forward(self, x: torch.Tensor, task_idx: torch.Tensor) -> torch.Tensor:
         if self.network is None:
             raise ValueError("Network is not configured.")
@@ -28,7 +114,18 @@ class MultiTaskStateTranslator(StateTranslator):
             y_norm_hat = y_norm_hat[..., self.loss_mask]
             y_norm = y_norm[..., self.loss_mask]
 
-        return torch.nn.functional.mse_loss(y_norm_hat, y_norm)
+        return F.mse_loss(y_norm_hat, y_norm)
+
+    def _parse_loss_mask(self) -> None:
+        mask = torch.zeros(self.network_config.output_dim, dtype=torch.bool)
+
+        if self.loss_mask_slices is not None:
+            for s in self.loss_mask_slices:
+                mask[parse_slice(s)] = True
+        else:
+            mask[:] = True
+
+        self.register_buffer("loss_mask", mask)
 
     def _configure_normalizers(self) -> None:
         dm = getattr(self.trainer, "datamodule", None)
