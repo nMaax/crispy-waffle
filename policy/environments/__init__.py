792f9b2913bcd0ca136d4e517ec8601ae459791e
nMaax <massimilianocarli@proton.me>
2026-05-16 12:16:36 +0200

    Added trailing _env to gymnasium environment files for clarity

--- policy/environments/__init__.py
+++ policy/environments/__init__.py
@@ -1,8 +1,8 @@
-from .place_sphere_panda_wristcam import PlaceSphereWristcamEnv
-from .place_sphere_panda_wristcam_restricted_spawn import PlaceSphereWristcamRestrictedSpawnEnv
-from .place_sphere_panda_wristcam_with_cubes import PlaceSphereWristcamWithCubesEnv
-from .stack_cube_swapped import StackCubeSwappedEnv
-from .stack_cube_with_sphere_and_bin import StackCubeWithSphereAndBinEnv
+from .place_sphere_panda_wristcam_env import PlaceSphereWristcamEnv
+from .place_sphere_panda_wristcam_restricted_spawn_env import PlaceSphereWristcamRestrictedSpawnEnv
+from .place_sphere_panda_wristcam_with_cubes_env import PlaceSphereWristcamWithCubesEnv
+from .stack_cube_swapped_env import StackCubeSwappedEnv
+from .stack_cube_with_sphere_and_bin_env import StackCubeWithSphereAndBinEnv
 
 __all__ = [
     "PlaceSphereWristcamEnv",
