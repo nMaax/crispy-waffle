9589079eef555db924d7a34dfd2cedfd4b70bcb8
nMaax <massimilianocarli@proton.me>
2026-05-26 14:39:38 +0200

    Added GC-DP to __init__ in algorithms

--- policy/algorithms/__init__.py
+++ policy/algorithms/__init__.py
@@ -1,6 +1,15 @@
 from .diffusion_policy import DiffusionPolicy
+from .goal_conditioned_diffusion_policy_egnn import GoalConditionedDiffusionPolicyEGNN
+from .goal_conditioned_diffusion_policy_mlp import GoalConditionedDiffusionPolicyMLP
 from .multi_task_state_aligner import MultiTaskStateAligner
 from .no_op import NoOp
 from .state_aligner import StateAligner
 
-__all__ = ["DiffusionPolicy", "MultiTaskStateAligner", "NoOp", "StateAligner"]
+__all__ = [
+    "DiffusionPolicy",
+    "GoalConditionedDiffusionPolicyEGNN",
+    "GoalConditionedDiffusionPolicyMLP",
+    "MultiTaskStateAligner",
+    "NoOp",
+    "StateAligner",
+]
