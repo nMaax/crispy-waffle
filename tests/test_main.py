a01c1bd7046abb31f8d03d820d15b805a0c4a8ec
nMaax <massimilianocarli@proton.me>
2026-07-26 16:37:06 +0200

    Added new experiments to the test suite

--- tests/test_main.py
+++ tests/test_main.py
@@ -45,6 +45,8 @@ experiment_commands_to_test: list[str | ParameterSet] = [
     # Goal Conditioned BESO
     "experiment=GoalConditionedBESO++__StackCube-v1__default__test trainer.max_epochs=1",
     "experiment=GoalConditionedBESO++__StackCube-v1__default__train trainer.max_epochs=1",
+    "experiment=GoalConditionedBESO++__StackCubeLockedRotation-v1__default__test trainer.max_epochs=1",
+    "experiment=GoalConditionedBESO++__StackCubeLockedRotation-v1__default__train trainer.max_epochs=1",
     "experiment=GoalConditionedBESO__StackCube-v1__default__test trainer.max_epochs=1",
     "experiment=GoalConditionedBESO__StackCube-v1__default__train trainer.max_epochs=1",
     "experiment=GoalConditionedBESO__StackCubeLockedRotation-v1__default__test trainer.max_epochs=1",
@@ -57,6 +59,8 @@ experiment_commands_to_test: list[str | ParameterSet] = [
     # Goal-Conditioned Diffusion Policy MLP
     "experiment=GoalConditionedDiffusionPolicyMLP__StackCube-v1__default__test trainer.max_epochs=1",
     "experiment=GoalConditionedDiffusionPolicyMLP__StackCube-v1__default__train trainer.max_epochs=1",
+    "experiment=GoalConditionedDiffusionPolicyMLP__StackCubeLockedRotation-v1__default__test trainer.max_epochs=1",
+    "experiment=GoalConditionedDiffusionPolicyMLP__StackCubeLockedRotation-v1__default__train trainer.max_epochs=1",
     # DiffusionPolicy variants (tuning)
     "experiment=DiffusionPolicy__PlaceCubeLeft-v1__default__tuning trainer.max_epochs=1",
 ]
